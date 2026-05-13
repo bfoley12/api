c9066d3b727933508cfcf94dcc3f692b3ec5c62f
Brendan Foley <brendanfoley1214@proton.me>
2026-05-13 16:35:57 -0700

    Removed unnecessary abstractions

--- mp_api/client/contribs/models/mpc.py
+++
@@ -1,7 +0,0 @@
-from __future__ import annotations
-
-
-# Brendan TODO: flesh out and allow classes to inherit from in order to convert to MPCDict
-class MPCLike:
-    def to_mpc(self) -> dict[str, str]:
-        pass
