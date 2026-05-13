a8acb75a9ced59679536179a0d867da4dc07d27f
Brendan Foley <brendanfoley1214@proton.me>
2026-05-12 21:52:33 -0700

    Added ProjectClient to manage project-related requests

--- mp_api/client/contribs/resources/project.py
+++ mp_api/client/contribs/resources/project.py
@@ -1,5 +1,7 @@
-from mp_api.client.contribs.client import ContribsClient
+from __future__ import annotations
 
+from mp_api.client.contribs.client import ContribsClient
+from typing import Any
 
 class ProjectClient:
     def __init__(self, client: ContribsClient):
@@ -9,3 +11,13 @@ class ProjectClient:
             client (ContribsClient): the parent client to manage connections
         """
         self.client = client
+
+    def get_project_by_name(self, name: str, fields: list[Any] | None):
+        params: dict[str, str | list[str]] = {}
+        if fields is not None:
+            params["_fields"] = ",".join(fields)
+        else:
+            fields = ["_all"]
+        r = self.client._http.get(f"/projects/{name}", params=params)
+        r.raise_for_status()
+        return Project.model_validate(r.json())
