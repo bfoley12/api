d9f8f0f94ef2a230d589eafb40e8db70b3d5217e
Brendan Foley <brendanfoley1214@proton.me>
2026-05-14 16:45:02 -0700

    Moved init_columns into ProjectResource

--- mp_api/client/contribs/resources/project.py
+++ mp_api/client/contribs/resources/project.py
@@ -1,23 +1,31 @@
 from __future__ import annotations
 
-from typing import Any, Protocol, cast
+from math import isclose
+from typing import Any, cast
 
 import httpx
+from pint.errors import DimensionalityError
 from pydantic import BaseModel
 
-from mp_api.client.contribs import helpers
+from mp_api.client.contribs import MPCC_SETTINGS, helpers
 from mp_api.client.contribs._logger import MPCC_LOGGER
+from mp_api.client.contribs._units import ureg
 from mp_api.client.contribs.models.project import ContribsProject
 from mp_api.client.contribs.models.reference import Reference
 from mp_api.client.contribs.models.response import Response
 from mp_api.client.contribs.pagination import paginate
-from mp_api.client.contribs.resources.base import VALID_RESOURCES, AsyncBaseResource
+from mp_api.client.contribs.resources.base import (
+    VALID_RESOURCES,
+    AsyncBaseProtocol,
+    AsyncBaseResource,
+)
 from mp_api.client.contribs.resources.mpc import format_output
+from mp_api.client.contribs.utils import flatten_dict, unflatten_dict
 from mp_api.client.core.exceptions import MPContribsClientError
 from mp_api.client.core.schemas import _DictLikeAccess
 
 
-class AsyncProjectProtocol(Protocol):
+class AsyncProjectProtocol(AsyncBaseProtocol):
     name: str | None
 
     async def get_project_by_name(
@@ -43,9 +51,19 @@ class AsyncProjectProtocol(Protocol):
         url: str,
     ) -> None: ...
 
-    async def update(self, update: dict[str, Any], name: str | None = None) -> None: ...
+    async def update(
+        self, update: dict[str, Any], name: str | None = None
+    ) -> ContribsProject: ...
 
     async def remove(self, name: str | None = None) -> None: ...
+
+    async def get_unique_identifiers_flags(
+        self, query: dict[str, Any] | None = None
+    ) -> dict[str, bool]: ...
+
+    async def init_columns(
+        self, columns: dict | None = None, name: str | None = None
+    ) -> ContribsProject: ...
 
 
 class AsyncProjectResource(AsyncBaseResource, AsyncProjectProtocol):
@@ -172,7 +190,9 @@ class AsyncProjectResource(AsyncBaseResource, AsyncProjectProtocol):
         else:
             raise MPContribsClientError(resp.get("error", resp))
 
-    async def update(self, update: dict[str, Any], name: str | None = None) -> None:
+    async def update(
+        self, update: dict[str, Any], name: str | None = None
+    ) -> ContribsProject:
         """Update project info.
 
         Args:
@@ -181,14 +201,16 @@ class AsyncProjectResource(AsyncBaseResource, AsyncProjectProtocol):
         """
         if not update:
             MPCC_LOGGER.warning("nothing to update")
-            return
+            raise MPContribsClientError("No update dict provided")
 
         name = self._get_name(name)
 
         disallowed = ["stats", "columns"]
         update = helpers.prune_dict(update, disallowed)
         if not update:
-            return
+            raise MPContribsClientError(
+                f"no valid keys given in udpate dict. Must not only contain {disallowed}"
+            )
 
         fields = list(ContribsProject.model_fields.keys())
         for k in disallowed:
@@ -215,6 +237,7 @@ class AsyncProjectResource(AsyncBaseResource, AsyncProjectProtocol):
         resp = await self.put(path=f"{name}", project=payload)
         if not resp.get("count", 0):
             raise MPContribsClientError(resp)
+        return resp
 
     # Named remove to avoid overriding the base.delete method, which is an http request
     async def remove(self, name: str | None = None) -> None:
@@ -232,3 +255,182 @@ class AsyncProjectResource(AsyncBaseResource, AsyncProjectProtocol):
         resp = await self.delete(pk=name)
         if resp and "error" in resp:
             raise MPContribsClientError(resp["error"])
+
+    async def get_unique_identifiers_flags(
+        self, query: dict[str, Any] | None = None
+    ) -> dict[str, bool]:
+        """Retrieve values for `unique_identifiers` flags.
+
+        See `client.available_query_params(resource="projects")` for available query parameters.
+
+        Args:
+            query (dict): query to select projects
+
+        Returns:
+            dict of str to bool, ex.:
+            {"<project-name>": True|False, ...}
+        """
+        results = await self.query(query=query, fields=["name", "unique_identifiers"])
+        return {p["name"]: p["unique_identifiers"] for p in results}
+
+    async def init_columns(
+        self, columns: dict | None = None, name: str | None = None
+    ) -> ContribsProject:
+        """Initialize columns for a project to set their order and desired units.
+
+        The `columns` field of a project tracks the minima and maxima of each `data` field
+        in its contributions. If columns are not initialized before submission using this
+        function, `submit_contributions` will respect the order of columns as they are
+        submitted and will try to auto-determine suitable units.
+
+        `init_columns` can be used at any point to reset the order of columns. Omitting
+        the `columns` argument will re-initialize columns based on the `data` fields of
+        all submitted contributions.
+
+        The `columns` argument is a dictionary which maps the data field names to its
+        units. Use `None` to indicate that a field is not a quantity (plain string). The
+        unit for a dimensionless quantity is an empty string (""). Percent (`%`) and
+        permille (`%%`) are considered units. Nested fields are indicated using a dot
+        (".") in the data field name.
+
+        Example:
+        >>> client.init_columns({"a": None, "b.c": "eV", "b.d": "mm", "e": ""})
+
+        This example will result in column headers on the project landing page of the form
+
+
+        |      |      data       |      |
+        | data |        b        | data |
+        |   a  | c [eV] | d [mm] | e [] |
+
+
+        Args:
+            columns (dict): dictionary mapping data column to its unit
+            name (str or None) : optional name of the project to use,
+                defaults to `self.project`.
+
+        Returns:
+            dict containing metadata about the column updates
+        """
+        name = self._get_name(name)
+
+        columns = flatten_dict(columns or {})
+
+        if len(columns) > MPCC_SETTINGS.MAX_COLUMNS:
+            raise MPContribsClientError(
+                f"Number of columns larger than {MPCC_SETTINGS.MAX_COLUMNS}!"
+            )
+
+        if not all(isinstance(v, str) for v in columns.values() if v is not None):
+            raise MPContribsClientError(
+                "All values in `columns` need to be None or of type str!"
+            )
+
+        new_columns = []
+
+        if columns:
+            # check columns input
+            scanned_columns = set()
+
+            # Gets valid column names into scanned_columns
+            for k, v in columns.items():
+                if k in MPCC_SETTINGS.COMPONENTS:
+                    scanned_columns.add(k)
+                    continue
+
+                nesting = k.count(".")
+                if nesting > MPCC_SETTINGS.MAX_NESTING:
+                    raise MPContribsClientError(
+                        f"Nesting depth larger than {MPCC_SETTINGS.MAX_NESTING} for {k}!"
+                    )
+
+                for col in scanned_columns:
+                    if nesting and col.startswith(k):
+                        raise MPContribsClientError(
+                            f"Duplicate definition of {k} in {col}!"
+                        )
+
+                    for n in range(1, nesting + 1):
+                        if k.rsplit(".", n)[0] == col:
+                            raise MPContribsClientError(
+                                f"Ancestor of {k} already defined in {col}!"
+                            )
+
+                is_valid_string = isinstance(v, str) and v.lower() != "nan"
+                if not is_valid_string and v is not None:
+                    raise MPContribsClientError(
+                        f"Unit '{v}' for {k} invalid (use `None` or a non-NaN string)!"
+                    )
+
+                if v != "" and v is not None and v not in ureg:
+                    raise MPContribsClientError(f"Unit '{v}' for {k} not supported!")
+
+                scanned_columns.add(k)
+
+            # sort to avoid "overlapping columns" error in handsontable's NestedHeaders
+            sorted_columns = flatten_dict(unflatten_dict(columns))
+            # also sort by increasing nesting for better columns display
+            sorted_columns = dict(
+                sorted(sorted_columns.items(), key=lambda item: item[0].count("."))
+            )
+
+            # TODO catch unsupported column renaming or implement solution
+            # reconcile with existing columns
+            resp = await self.get_project_by_name(name=name, fields=["columns"])
+            existing_columns = {}
+
+            for col in resp.columns:
+                existing_columns[col.path] = col.model_dump().pop("path")
+
+            for path, unit in sorted_columns.items():
+                if path in MPCC_SETTINGS.COMPONENTS:
+                    new_columns.append({"path": path})
+                    continue
+
+                full_path = f"data.{path}"
+                new_column = {"path": full_path}
+                existing_column = existing_columns.get(full_path)
+
+                if unit is not None:
+                    new_column["unit"] = unit
+
+                if existing_column:
+                    # NOTE if existing_unit == "NaN":
+                    #   it was set by omitting "unit" in new_column
+                    new_unit = new_column.get("unit", "NaN")
+                    existing_unit = existing_column.get("unit")
+                    if existing_unit != new_unit:
+                        if existing_unit == "NaN" and new_unit == "":
+                            factor = 1
+                        else:
+                            conv_args = []
+                            for u in [existing_unit, new_unit]:
+                                try:
+                                    conv_args.append(ureg.Unit(u))
+                                except ValueError:
+                                    raise MPContribsClientError(
+                                        f"Can't convert {existing_unit} to {new_unit} for {path}"
+                                    )
+                            try:
+                                factor = ureg.convert(1, *conv_args)  # type: ignore[arg-type]
+                            except DimensionalityError:
+                                raise MPContribsClientError(
+                                    f"Can't convert {existing_unit} to {new_unit} for {path}"
+                                )
+
+                        if not isclose(factor, 1):
+                            MPCC_LOGGER.info(
+                                f"Changing {existing_unit} to {new_unit} for {path} ..."
+                            )
+                            # TODO scale contributions to new unit
+                            raise MPContribsClientError(
+                                "Changing units not supported yet. Please resubmit"
+                                " contributions or update accordingly."
+                            )
+
+                new_columns.append(new_column)
+
+        payload = {"columns": new_columns}
+        self._is_valid_payload(cast(BaseModel, ContribsProject), payload)
+
+        return await self.update(update=payload, name=name)
